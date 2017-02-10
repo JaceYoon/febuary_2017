822a7792eead061ef47529468f353501b8a0be26
Jace Yoon <jace.jihoon.yoon@gmail.com>
2017-02-10 14:54:02 -0800

    added Jace's work

---
+++ JaceYoon/friends/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app,'friendsdb')
+@app.route('/')
+def index():
+    friends = mysql.query_db("SELECT * FROM friends")
+    print friends
+    return render_template('index.html')
+@app.route('/friends', methods=['POST'])
+def create():
+     # Write query as a string. Notice how we have multiple values
+    # we want to insert into our query.
+    query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())"
+    # We'll then create a dictionary of data from the POST data received.
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation']
+           }
+    # Run query, with dictionary values injected into the query.
+    mysql.query_db(query, data)
+    return redirect('/')
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation WHERE id = :id"
+    data = {
+             'first_name': request.form['first_name'],
+             'last_name':  request.form['last_name'],
+             'occupation': request.form['occupation'],
+             'id': friend_id
+           }
+    mysql.query_db(query, data)
+    return render_template('index.html', one_friend=friends[0])
+@app.route('/remove_friend/<friend_id>', methods=['POST'])
+def delete(friend_id):
+    query = "DELETE FROM friends WHERE id = :id"
+    data = {'id': friend_id}
+    mysql.query_db(query, data)
+    return redirect('/')
+app.run(debug=True)
